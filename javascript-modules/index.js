c87305692c4bdd6aa42ed3dd1f828a297bc47cf0
radityaabi <radityaabiansyah@gmail.com>
2025-11-10 19:50:22 +0700

    create react project

--- javascript-modules/index.js
+++
@@ -1,4 +0,0 @@
-import { sum, multiply } from "./lib.js";
-
-console.log(sum(2, 3));
-console.log(multiply(4, 5));
--- javascript-modules/lib.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-export { sum, multiply };
