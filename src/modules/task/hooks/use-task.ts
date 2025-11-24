1cd1bee1d8bbb985b599b680ae1275734ede3390
radityaabi <radityaabiansyah@gmail.com>
2025-11-24 20:13:46 +0700

    fix : fix target date when add task

--- src/modules/task/hooks/use-task.ts
+++ src/modules/task/hooks/use-task.ts
@@ -69,6 +69,9 @@ export const useTasks = (
       const newTask: Task = {
         ...validatedData,
         id: lastId + 1,
+        targetDate: validatedData.targetDate
+          ? new Date(validatedData.targetDate)
+          : undefined,
         createdAt: new Date(),
         updatedAt: new Date(),
       };
